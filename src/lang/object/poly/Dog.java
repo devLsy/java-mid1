df274338e2e7dcda334a84d0b339d018212df48d
이상엽 <infightersy@naver.com>
2024-10-28 16:46:17 +0900

    커밋

---
+++ src/lang/object/poly/Dog.java
@@ -0,0 +1,7 @@
+package lang.object.poly;
+
+public class Dog {
+    public void sound() {
+        System.out.println("개 이동");
+    }
+}
---
+++ src/lang/object/tostring/ToStringMain1.java
@@ -0,0 +1,13 @@
+package lang.object.tostring;
+
+public class ToStringMain1 {
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+        String string = obj.toString();
+
+        System.out.println(string);
+
+        System.out.println(obj);
+    }
+}
