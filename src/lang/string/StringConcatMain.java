f3f8cf8785fcf6c8302c32282297f282f907958f
이상엽 <infightersy@naver.com>
2024-11-18 07:53:52 +0900

    추가

--- src/lang/string/StringConcatMain.java
+++ src/lang/string/StringConcatMain.java
@@ -4,7 +4,7 @@ public class StringConcatMain {
 
     public static void main(String[] args) {
         String a = "hello";
-        String b = "java";
+        String b = " java";
 
         String result1 = a.concat(b);
         String result2 = a + b;
